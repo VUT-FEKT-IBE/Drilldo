0ce1eeedfc77d032060f9cec893544c9e6ed963f
czechbol <adamludes@gmail.com>
2023-01-02 22:42:08 +0100

    set page title

--- tester/src/router/index.js
+++ tester/src/router/index.js
@@ -7,7 +7,10 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: TestView
+      component: TestView,
+      meta: {
+      title: 'FEKT Tester'
+    }
     },
     {
       path: '/about',
@@ -15,9 +18,22 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+      title: 'FEKT Tester | About'
+    }
     }
   ]
 })
-
+router.beforeEach((to, from, next) => {
+  // Get the page title from the route meta data that we have defined
+  // See further down below for how we setup this data
+  const title = to.meta.title
+// If the route has a title, set it as the page title of the document/page
+  if (title) {
+    document.title = title
+  }
+  // Continue resolving the route
+  next()
+})
 export default router
