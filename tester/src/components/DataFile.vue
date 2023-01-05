55cfb23303a03d646cb0434844d05211b3755023
Martin Nečas <necas.marty@gmail.com>
2023-01-05 21:21:24 +0100

    Data selector

---
+++ tester/src/components/DataFile.vue
@@ -0,0 +1,40 @@
+<script setup>
+const emit = defineEmits(["data"]);
+const props = defineProps(["name", "filename"]);
+
+function onFileUpload($event) {
+    fetch('data/' + props.filename)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json){
+            console.log(json)
+            emit("data", json);
+        })
+        .catch(function(err) {
+            console.log(`Error: ${err}` )
+        });
+}
+</script>
+
+<template>
+  <div>
+    <button class="but-file-upload" @click="onFileUpload()">
+      <span class="but-text">Select {{ props.name }}</span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.but-file-upload {
+  display: flex;
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin: 2px;
+  width: 100%;
+  text-align: center;
+  border: 0px;
+  background-color: #2a9d8f;
+  color: white;
+}
+</style>
