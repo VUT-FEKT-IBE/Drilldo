7b14340ee5e84d8f263ac9b3fdf292d553007111
czechbol <adamludes@gmail.com>
2023-01-07 20:15:20 +0100

    PWA time

--- tester/vite.config.js
+++ tester/vite.config.js
@@ -2,11 +2,64 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/Drilldo",
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      base: "/Drilldo/",
+      manifest: {
+        name: "Drilldo",
+        short_name: "Drilldo",
+        description: "Question set drill app",
+        display: "standalone",
+        background_color: "#012A4A",
+        start_url: ".",
+        theme_color: "#012A4A",
+        icons: [
+          {
+            src: "/Drilldo/icon-x512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+          {
+            src: "/Drilldo/icon-x384.png",
+            type: "image/png",
+            sizes: "384x384",
+          },
+          {
+            src: "/Drilldo/icon-x192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/Drilldo/icon-x128.png",
+            type: "image/png",
+            sizes: "128x128",
+          },
+          {
+            src: "/Drilldo/icon-x96.png",
+            type: "image/png",
+            sizes: "96x96",
+          },
+          {
+            src: "/Drilldo/icon-x72.png",
+            type: "image/png",
+            sizes: "72x72",
+          },
+          {
+            src: "/Drilldo/icon-x48.png",
+            type: "image/png",
+            sizes: "48x48",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/Drilldo/",
   build: {
     emptyOutDir: true,
     outDir: "../docs",
